a9da5dfaa9fcd00098d445a79bc13a9152d6440d
Chad Kittel <chad.kittel@gmail.com>
2013-04-20 12:18:18 -0500

    Of course I would have done that, *resetting password* :)

--- src/SkytapSharp/SkytapSharp.Console/Program.cs
+++ src/SkytapSharp/SkytapSharp.Console/Program.cs
@@ -4,11 +4,10 @@
     {
         static void Main(string[] args)
         {
-            var client = new SkytapClient("ckittel", "AN8uyQZAXD/2;4CHAC");
+            var client = new SkytapClient("yourUser", "yourKey");
 
             // Scratch pad for trying out the client
-            var result = client.GetConfiguration("782260");
-            System.Console.WriteLine(result.Name);
+           
 
         }
     }
