9a8041c87b3dd24efa3704c69519c77650844f8b
Chad Kittel <chad.kittel@gmail.com>
2013-04-21 17:27:38 -0500

    work on the quota API

--- src/SkytapSharp/SkytapSharp/Models/Quota.cs
+++ src/SkytapSharp/SkytapSharp/Models/Quota.cs
@@ -1,11 +1,60 @@
 ﻿namespace SkytapSharp.Models
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Quota
     {
+        /// <summary>
+        /// Gets the identifier for the quota
+        /// 
+        /// This is RO in the API
+        /// </summary>
         public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the usage limit for the user.
+        /// 
+        /// What happens when the quota is reached depends on the quota type.
+        ///  * When the SVM hours quota is reached, all running VMs are suspended.
+        ///    Using more SVM hours required an increase in the quota limit.
+        ///  * When the SVM quota is reached, no futher VMs can be started.  Stopping
+        ///    running VMs "frees up" SVMs and allows more to be started.
+        ///  * When the storage quota is reach, no more templates or configurations
+        ///    may be created.  Deleting existing templates or configurations allows
+        ///    more to be made.
+        /// 
+        /// This is RW in the API
+        /// </summary>
         public string Limit { get; set; }
+
+        /// <summary>
+        /// Gets what kind of resource the quota is measuring type.  
+        /// One of:
+        ///  * concurrent_storage_size: MB/GB - Storage limits
+        ///  * concurrent_svms: int - The number of Skytap VMs that may be used at the same time
+        ///  * cumulative_svms: int - The number of hours of SVM usage (ie, SVM hours)
+        /// 
+        /// This is RO in the API
+        /// </summary>
         public string QuotaType { get; set; }
+
+        /// <summary>
+        /// Gets the units related to the Limit and Usage fields.  The
+        /// value of this is QuotaType specific.
+        /// 
+        /// This is RO in the API
+        /// </summary>
         public string Units { get; set; }
+
+        /// <summary>
+        /// Gets the current usage information against the quota.  It is not required for every
+        /// user to have a quota defined for all or even any quota type.  In case a user does
+        /// not have a quota of a particular type, the quota for the customer account is used
+        /// by default.
+        /// 
+        /// This is RO in the API
+        /// </summary>
         public decimal Usage { get; set; }
     }
 }
--- src/SkytapSharp/SkytapSharp/Users.cs
+++ src/SkytapSharp/SkytapSharp/Users.cs
@@ -36,5 +36,54 @@ namespace SkytapSharp
             // POST
             throw new NotImplementedException();
         }
+
+        /// <summary>
+        /// Gets a list of quotas defined for the user
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public IReadOnlyCollection<Quota> GetUserQuotas(string userId)
+        {
+            var request = new RestRequest { Resource = UserResource + "/" + userId + "/quotas" };
+
+            return Execute<List<Quota>>(request).AsReadOnly();
+        }
+
+        /// <summary>
+        /// Creates a new user quota.  When creating a quota, the Type and Limit
+        /// fields are required.  The Units and Usage fields are ignored.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="newQuota"></param>
+        /// <returns></returns>
+        public Quota CreateUserQuota(string userId, Quota newQuota)
+        {
+            // POST
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Updates a user quota
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="quotaId"></param>
+        /// <param name="newLimit"></param>
+        /// <returns></returns>
+        public Quota ChangeUserQuotaLimit(string userId, string quotaId, int newLimit)
+        {
+            // PUT
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Removes a user-specific quota.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="quotaId"></param>
+        public void DeleteUserQuota(string userId, string quotaId)
+        {
+            // DELETE
+            throw new NotImplementedException();
+        }
     }
 }
